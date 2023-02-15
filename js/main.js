b15a7a30f105193a25704ee2f0975907524ad712
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-02-15 18:06:53 +0200

    footer done, cadillac left

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
+$(function () {
+
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+    })
 
 });
